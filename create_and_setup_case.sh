100200f29a3bedaf8e08916f1ab049aa09862c8b
Michael Levy <mike.levy.work@gmail.com>
2022-07-06 15:33:28 -0500

    Support for 4p2z setup

--- create_and_setup_case.sh
+++ create_and_setup_case.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# NOTE: To get email notifications from run, set up ~/.cime/config
 # exit on error
 set -e
 
 # USER CONFIGURABLE OPTIONS
-CASENAME=g.e22.TL319_t13.G1850ECOIAF_JRA_HR.test_15x15.001
+CASENAME=g.e22.TL319_t13.G1850ECOIAF_JRA_HR.test_15x15.4p2z.ndep_shr_stream
 CASEROOT_PARENT=/work2/08815/mlevy/frontera/codes/CESM/cesm2.2.0/cases
 
 # RECOMMEND KEEPING THESE UNCHANGED
@@ -18,13 +19,13 @@ ref_dir=/work2/08815/mlevy/frontera/high_res_inputs/restarts
 ref_case=g.e20.G.TL319_t13.control.001_contd
 ref_date=0245-01-01
 # 1958 - 1995 [245-282; 1-38]:
-tavg_contents_override_file=/work2/08815/mlevy/frontera/high_res_inputs/tx0.1v3_tavg_contents_no5day
+tavg_contents_override_file=/work2/08815/mlevy/frontera/codes/frontera_highres_scripts/tavg_contents/tx0.1v3_tavg_contents_no5day_4p2z
 n_tavg_streams=3
 # 1996 - 2018 [283-305; 39-61]:
 # Alternate for Yassir: use 5day output for last 10 years => turn on 2009 (52)
 # tavg_contents_override_file=/work2/08815/mlevy/frontera/high_res_inputs/tx0.1v3_tavg_contents
 # n_tavg_streams=4
-init_ecosys_init_file="/work2/08815/mlevy/frontera/high_res_inputs/ecosys_jan_IC_g.e22b05.G1850ECOIAF_JRA.TL319_g17.cocco.001_0123-01-01_POP_tx0.1v3_c200720.nc"
+init_ecosys_init_file="/work2/08815/mlevy/frontera/high_res_inputs/ecosys_jan_IC_g.e22.GOMIPECOIAF_JRA-1p4-2018.TL319_g17.4p2z.001_0306-01-01_POP_tx0.1v3_c220701.nc"
 
 
 # 1. Create case
@@ -51,11 +52,12 @@ echo "Making XML changes..."
 #./xmlchange NTASKS_OCN=22626
 ./xmlchange NTASKS_OCN=25654
 ./xmlchange OCN_CHL_TYPE=prognostic
-./xmlchange OCN_BGC_CONFIG=latest+cocco
+#./xmlchange OCN_BGC_CONFIG=latest+cocco # for 4p2z, will use user_nl_marbl
 ./xmlchange RUN_TYPE=hybrid,RUN_REFCASE=${ref_case},RUN_REFDATE=${ref_date}
 ./xmlchange OCN_TRACER_MODULES=ecosys
 ./xmlchange -a CICE_CONFIG_OPTS="-trage 0"
-./xmlchange DATM_PRESAERO=clim_2000,DATM_MODE=CORE_IAF_JRA,DROF_MODE=IAF_JRA
+./xmlchange DATM_MODE=CORE_IAF_JRA,DROF_MODE=IAF_JRA
+./xmlchange STOP_N=1,STOP_OPTION=nmonths
 
 # 2.5 Additional XML Changes that Keith noticed:
 #     * Change CPL_SEQ_OPTION
@@ -102,6 +104,14 @@ ltidal_mixing = .true.
 dt_count = 816
 time_mix_freq = 17
 
+! ndep from shr_stream
+ndep_data_type = 'shr_stream'
+ndep_shr_stream_file = '/scratch1/08815/mlevy/tmp_inputdata/ocn_Ndep_transient_forcing_x0.1_220705.nc'
+ndep_shr_stream_scale_factor = 7.1429e+06
+ndep_shr_stream_year_align = 0
+ndep_shr_stream_year_first = 1652
+ndep_shr_stream_year_last = 2019
+
 ! First run needs initial conditions for ecosys
 init_ecosys_option='ccsm_startup'
 init_ecosys_init_file = '${init_ecosys_init_file}'
@@ -136,7 +146,6 @@ f_vicen = "mxxxx"
 f_vsnon = "mxxxx"
 EOF
 
-
 ## Switch to JRA v1.5_noleap
 cat >> user_nl_datm << EOF
   streams = "datm.streams.txt.CORE_IAF_JRA.GCGCS.PREC 245 1958 2021",
@@ -146,13 +155,15 @@ cat >> user_nl_datm << EOF
       "datm.streams.txt.CORE_IAF_JRA.NCEP.SLP_ 245 1958 2021",
       "datm.streams.txt.CORE_IAF_JRA.NCEP.T_10 245 1958 2021",
       "datm.streams.txt.CORE_IAF_JRA.NCEP.U_10 245 1958 2021",
-      "datm.streams.txt.CORE_IAF_JRA.NCEP.V_10 245 1958 2021",
-      "datm.streams.txt.presaero.clim_2000 1 1 1"
+      "datm.streams.txt.CORE_IAF_JRA.NCEP.V_10 245 1958 2021"
 EOF
 
 cat >> user_nl_drof << EOF
   streams = "drof.streams.txt.rof.iaf_jra 1 1958 2021"
 EOF
+
+# user_nl_marbl is a big file, don't want to include it all in this script
+cp ${USER_STREAM_DIR}/../user_nl/user_nl_marbl .
 
 # Copy user_*.streams.txt.* to case directory
 cp ${USER_STREAM_DIR}/user_* .
