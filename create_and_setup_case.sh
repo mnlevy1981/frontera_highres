b0759ac9265f62b59fc58ca7f19ae976c5e2e34e
Michael Levy <mike.levy.work@gmail.com>
2022-07-10 09:16:04 -0500

    Copy files from repo dir, not Mike's checkout
    Determined location of create_and_setup_case.sh by running "dirname $0",
    and set USER_STREAM_DIR, SOURCEMOD_DIR, and location of
    tavg_contents_override_file accordingly. Also updated ndep stream
    filename.

--- create_and_setup_case.sh
+++ create_and_setup_case.sh
@@ -5,21 +5,22 @@
 set -e
 
 # USER CONFIGURABLE OPTIONS
-CASENAME=g.e22.TL319_t13.G1850ECOIAF_JRA_HR.test_15x15.4p2z.ndep_shr_stream
+CASENAME=g.e22.TL319_t13.G1850ECOIAF_JRA_HR.test_12x12.4p2z.ndep_shr_stream.002
 CASEROOT_PARENT=/work2/08815/mlevy/frontera/codes/CESM/cesm2.2.0/cases
 
 # RECOMMEND KEEPING THESE UNCHANGED
+SCRIPT_DIR=`cd -P $(dirname $0) ; echo $PWD`
 CESMROOT=/work2/08815/mlevy/frontera/codes/CESM/cesm2.2.0
 COMPSET=G1850ECOIAF_JRA_HR
 #COMPSET="2000_DATM%JRA_SLND_CICE_POP2_DROF%JRA_SGLC_SWAV"
 RES=TL319_t13
-USER_STREAM_DIR=/work2/08815/mlevy/frontera/codes/frontera_highres_scripts/user_streams
-SOURCEMOD_DIR=/work2/08815/mlevy/frontera/codes/frontera_highres_scripts/SourceMods
+USER_STREAM_DIR=${SCRIPT_DIR}/user_streams
+SOURCEMOD_DIR=${SCRIPT_DIR}/SourceMods
 ref_dir=/work2/08815/mlevy/frontera/high_res_inputs/restarts
 ref_case=g.e20.G.TL319_t13.control.001_contd
 ref_date=0245-01-01
 # 1958 - 1995 [245-282; 1-38]:
-tavg_contents_override_file=/work2/08815/mlevy/frontera/codes/frontera_highres_scripts/tavg_contents/tx0.1v3_tavg_contents_no5day_4p2z
+tavg_contents_override_file=${SCRIPT_DIR}/tavg_contents/tx0.1v3_tavg_contents_no5day_4p2z
 n_tavg_streams=3
 # 1996 - 2018 [283-305; 39-61]:
 # Alternate for Yassir: use 5day output for last 10 years => turn on 2009 (52)
@@ -113,7 +114,7 @@ time_mix_freq = 17
 
 ! ndep from shr_stream
 ndep_data_type = 'shr_stream'
-ndep_shr_stream_file = '/scratch1/08815/mlevy/tmp_inputdata/ocn_Ndep_transient_forcing_x0.1_220705.nc'
+ndep_shr_stream_file = '/scratch1/08815/mlevy/tmp_inputdata/ocn_Ndep_transient_forcing_x0.1_220709.nc'
 ndep_shr_stream_scale_factor = 7.1429e+06
 ndep_shr_stream_year_align = 0
 ndep_shr_stream_year_first = 1652
@@ -189,6 +190,10 @@ run_dir=`./xmlquery RUNDIR --value`
 echo "copying restart files and rpointer files to ${run_dir}..."
 
 cp -v ${ref_dir}/${ref_date}-00000/* ${run_dir}
+
+echo ""
+echo "SUCCESS! Created ${CASEROOT_PARENT}/${CASENAME}"
+echo "cd to that directory and run 'idev' to get a compute node, then run ./case.build"
 
 # Next run "idev" to get interactive node, and then run ./case.build
 # (from what I can tell, srun will launch 56 concurrent instances of case.build)
