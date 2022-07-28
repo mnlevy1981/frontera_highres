0892236817a6da3fa848d1e893b8b7a307b8ac5c
Kristen Krumhardt <kristen.krumhardt@colorado.edu>
2022-07-28 10:22:49 -0600

    add regridding notebooks

---
+++ gen_IC_Ndep_files/config.py
@@ -0,0 +1,14 @@
+import os
+
+USER = os.environ['USER']
+
+DATAROOT = f'/glade/work/{USER}'
+
+inputdata = '/glade/p/cesmdata/cseg/inputdata'
+
+dirwork = f'{DATAROOT}/cesm_inputdata/work'
+os.makedirs(dirwork, exist_ok=True)   
+
+dirout = f'{DATAROOT}/cesm_inputdata'
+dirout = f'/glade/scratch/kristenk/cesm_inputdata'
+os.makedirs(dirout, exist_ok=True)   
